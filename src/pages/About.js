218251290df8252a2762826535361229c63a8a9c
Prajwol <prajwol-gautam@emblocktech.com>
2025-12-26 12:15:26 +0530

    about us op photo

--- src/pages/About.js
+++ src/pages/About.js
@@ -129,12 +129,17 @@ function About() {
       <div className="py-16">
         <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
           <h2 className="text-3xl font-bold text-center mb-12">Our Leadership Team</h2>
-          <div className="grid grid-cols-1 md:grid-cols-1 gap-8">
+          <div className="grid grid-cols-1 md:grid-cols-2 gap-8">
             {[
               {
                 name: "Santhosh Srinivas",
                 role: "Founder & CEO",
                 image: "/owner.JPG"
+              },
+              {
+                name: "Gopi krishna D",
+                role: "Operations Manager",
+                image: "/gopi-krishna.jpg"
               }
               /* {
                 name: "Roshan Vennavalli",
